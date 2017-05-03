4262fc5e2526be54cab55ad93c8abbd907c2e231
Pablosensi <Pablosensi@Pabsensi>
2017-05-03 19:46:05 +0200

    Added Account hashmap for the User class, and added methods for adding and deducing funds from each account. #B1710-36

--- BankApp/src/main/java/bankapp/server/User.java
+++ BankApp/src/main/java/bankapp/server/User.java
@@ -1,5 +1,7 @@
 package bankapp.server;
 
+import java.util.HashMap;
+
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.PrimaryKey;
 
@@ -9,16 +11,28 @@ public class User {
 	private String username;
 	private String pass;
 	private String email;
+	private HashMap<String, Account> accounts;
 	public User(String username, String pass, String email){
 		this.username = username;
 		this.pass = pass;
 		this.email = email;
+		this.accounts = new HashMap<>();
+	}
+	public void deduceAccountFunds(String accNum, int money){
+		this.accounts.get(accNum).deducemoney(money);
+	}
+	public void addFundstoAccount(String accNum, int money){
+		this.accounts.get(accNum).addmoney(money);
+	}
+	public void createAccount(){
+		Account acc = new Account();
+		addAccount(acc);
+	}
+	public void addAccount(Account acc){
+		this.accounts.put(Integer.toString(acc.getnumAcc()), acc);
 	}
 	public String getUsername() {
 		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
 	}
 	public String getPass() {
 		return pass;

8ab52d9078847ab14cfbc9ae84bf0026a550155a
Pablosensi <Pablosensi@Pabsensi>
2017-05-03 20:06:39 +0200

    Added loop to the bankClient to allow the extension of client-side functionalities. #B1710-36

--- BankApp/src/main/java/bankapp/client/bankClient.java
+++ BankApp/src/main/java/bankapp/client/bankClient.java
@@ -6,13 +6,17 @@ public class bankClient {
 
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		System.out.println("Please insert username login");
 		Scanner keyboard = new Scanner(System.in);
-	    String username = keyboard.nextLine();
+		bankController bController = new bankController(args[0], args[1], args[2]);
+	    String username;
+	    String pass;
+		do{
+		System.out.println("Please insert username login");
+	    username = keyboard.nextLine();
 	    System.out.println("Please insert user pass");
-	    String pass = keyboard.nextLine();
-	    bankController bController = new bankController(args[0], args[1], args[2]);
-	    bController.login(username, pass);
+	    pass = keyboard.nextLine();
+		}while(!bController.login(username, pass));
+		
 	}
 
 }

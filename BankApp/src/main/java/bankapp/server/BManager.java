a2b919deb80fa417ae66d007e7c30c5d35fecb65
Diana ARTIOM <cristina.stratan@opendeusto.es>
2017-05-06 17:57:38 +0200

    Changet system.out to logging.

--- BankApp/src/main/java/bankapp/server/BManager.java
+++ BankApp/src/main/java/bankapp/server/BManager.java
@@ -2,12 +2,17 @@ package bankapp.server;
 
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class BManager extends UnicastRemoteObject implements IBManager {
 
 	/**
 	 * 
 	 */
+
+	private static final Logger LOGGER = Logger.getLogger(BManager.class.getName() );
+
 	private static final long serialVersionUID = 1L;
 
 	bankDAO bd = new bankDAO();
@@ -27,16 +32,16 @@ public class BManager extends UnicastRemoteObject implements IBManager {
 		try{
 			User login = bd.getUser(username);
 			if(login.getPass().equals(pass)){
-				System.out.println("-- User : " + username + " // Pass : " + pass + " --");
-				System.out.println("login successful!");
+				LOGGER.log(Level.FINE, "-- User : " + username + " // Pass : " + pass + " --");
+				LOGGER.log(Level.FINE, "login successful!");
 			return true;
 			}
 			else{
-				System.out.println("Error: found user but password does not coincide");
+				LOGGER.log(Level.SEVERE, "Error: found user but password does not coincide");
 				return false;
 			}
 		}catch(Exception ex){
-			System.out.println("Error retrieving user from DB. Check if the user exists");
+			LOGGER.log(Level.SEVERE, "Error retrieving user from DB. Check if the user exists");
 			return false;
 		}
 	}

7a04972c2336d39c60118de39cc4e5258ba3ba02
pabloasensi <pabsensi@opendeusto.es>
2017-05-09 02:34:16 +0200

    added UserUT

---
+++ BankApp/src/test/java/bankapp/server/UserUT.java
@@ -0,0 +1,45 @@
+package bankapp.server;
+
+import bankapp.server.User;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import javax.validation.*;
+import java.util.Set;
+
+public class UserUT {
+
+    protected static Validator validator;
+
+    @Before
+    public void setup() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    public void validUser() {
+        User user = new User("username", "password", "email@example.com");
+        assertTrue(user != null);
+        assertThat(user.getEmail(), is("email@example.com"));
+        assertThat(user.getPass(), is("password"));
+        assertThat(user.getUsername(), is("username"));
+    }
+
+    /* TODO - review validation fields annotations */
+    @Test(expected = ConstraintViolationException.class)
+    public void invalidUser() {
+        User user = new User("", "", "");
+        assertTrue(user != null);
+        assertFalse(validate(user).isEmpty());
+    }
+
+    public static <T> Set<ConstraintViolation<T>> validate(T obj) {
+        return validator.validate(obj);
+    }
+}

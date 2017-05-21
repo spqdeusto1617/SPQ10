0949e3db84420019196faa71f5949c92e78cd215
pabloasensi <pabsensi@opendeusto.es>
2017-05-21 23:26:44 +0200

    added more missing code

--- src/main/java/bankapp/client/BankController.java
+++ src/main/java/bankapp/client/BankController.java
@@ -10,19 +10,27 @@ public class bankController {
 		//new Login(this);
 		rsl.setService(ip, port, serviceName);
 	}
-	
-	public boolean login(String user, String pass){
+	public char login(String user, String pass){
+		//calls the service locator login method from the controller
 		System.out.println("Controller calling -> RMIServiceLocator");
-		if(rsl.login(user, pass)){
-			//new Booking_Search(this);
-			System.out.println("Login Succesfull");
-			return true;
-		}
-		else{
-			System.out.println("Login Unsuccessfull");
-			return false;
-		}
-	
-
+		return rsl.login(user, pass);
+	}
+	public void createUser(String user, String pass, String email){
+		rsl.createUser(user, pass, email);
+	}
+	public void transaction(String user1, String user2, long money, String accNum1, String accNum2){
+		rsl.transaction(user1, user2, money, accNum1, accNum2);
+	}
+	public int createBankAccount(String user){
+		return rsl.createBankAccount(user);
+	}
+	public void addFunds(String user, String accNum, long money){
+		rsl.addFunds(user, accNum, money);
+	}
+	public void deleteBankAccount(String user, String accNum1, String accNum2){
+		rsl.deleteBankAccount(user, accNum1, accNum2);
+	}
+	public void deleteAccount(String user){
+		rsl.deleteAccount(user);
 	}
 }
